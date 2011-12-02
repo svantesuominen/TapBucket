d7818cbab7780ef161e9335c9f5205e5726355b8
Svante Suominen <svante@svante.fi>
2011-12-02 20:58:27 +0200

    RubyOnRails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TapBucket::Application.config.secret_token = '7063b469e725c83d59ce235a1d6afaa30f4bcc741ea394587da83d6eebab66ee46fb79a42f9c0cee969440647c7e21617962aab7baec10fb5f97570979bbd2a2'
